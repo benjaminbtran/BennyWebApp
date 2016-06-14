47feb0222476afde287ec7888681e418198759d8
BennyTran <benjaminb.tran@gmail.com>
2016-06-14 15:52:56 -0700

    New project files

---
+++ Benny.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Benny.Web.Startup))]
+namespace Benny.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
